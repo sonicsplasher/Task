087ffa8dc1a80a5542983467810259d1e565c9eb
Manuka Koushik Yadav <46778240+sonicsplasher@users.noreply.github.com>
2021-12-10 16:34:40 +0530

    FireBase Changes

---
+++ app/src/main/java/com/devjam/task/MainLoad.kt
@@ -0,0 +1,12 @@
+package com.devjam.task
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainLoad : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_load)
+    }
+}
+
