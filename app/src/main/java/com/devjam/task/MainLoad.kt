e957692bbde7e6a638182cce4c10e24a66702335
Manuka Koushik Yadav <46778240+sonicsplasher@users.noreply.github.com>
2021-12-11 14:08:31 +0530

    UI AMENDS

--- app/src/main/java/com/devjam/task/MainLoad.kt
+++ app/src/main/java/com/devjam/task/MainLoad.kt
@@ -1,12 +1,25 @@
 package com.devjam.task
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
+import android.os.CountDownTimer
+
+
+
 
 class MainLoad : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_load)
-    }
-}
+        object : CountDownTimer(5000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {}
+            override fun onFinish() {
+                //set the new Content of your activity
+                val intent = Intent(this, Login::class.java)
+            }
+
+    }.start()
+}}
 
