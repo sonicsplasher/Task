75f6faf59e0106292a905f9fae1ba7b0c5cc0619
Manuka Koushik Yadav <46778240+sonicsplasher@users.noreply.github.com>
2021-12-11 10:09:42 +0530

    1.1.7

--- app/src/main/java/com/devjam/task/Login.kt
+++ app/src/main/java/com/devjam/task/Login.kt
@@ -10,15 +10,12 @@ class Login : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-        auth = Firebase.auth
     }
     public override fun onStart() {
         super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = auth.currentUser
-        if(currentUser != null){
+
+
+
 
         }
     }
-}
